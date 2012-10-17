f775c5b15c32650acfceea2aeb8837e80d656218
Zachary Bush <zach@zmbush.com>
2012-10-17 11:11:20 -0700

    Add new file types

--- lib/conf.py
+++ lib/conf.py
@@ -1,3 +1,22 @@
 class Configuration:
   def __init__(self, filename):
-    pass
+    self.filename = filename
+    try:
+      self.confFile = open(filename, 'r')
+    except IOError as e:
+      self.interactiveConf()
+
+  def interactiveConf(self):
+    end = False
+    while not end:
+      print
+      print "What kind of account would you like to add:"
+      print "(T)witter"
+      print "(F)acebook"
+      print "(E)xit"
+      print
+      c = raw_input("> ").upper()
+      if c == "T":
+        pass
+      if c == "E":
+        end = True
